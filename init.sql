ee89346291b84763ddab4fc9f1180e9066b1d192
Andrei Stan <andreistan2003@gmail.com>
2026-04-26 16:23:12 +0300

    Add routes trips & reservations + change user -> app_user

--- init.sql
+++ init.sql
@@ -1,7 +1,30 @@
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS app_user (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     user_type VARCHAR(20) DEFAULT 'Adult'
 );
 
+CREATE TABLE IF NOT EXISTS route (
+    id SERIAL PRIMARY KEY,
+    from_location TEXT NOT NULL,
+    to_location TEXT NOT NULL,
+    leave_at TIME NOT NULL,
+    arrive_by TIME NOT NULL,
+    price INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS trip (
+    id SERIAL PRIMARY KEY,
+    route_id INT NOT NULL REFERENCES route(id) ON DELETE CASCADE,
+    trip_day DATE NOT NULL,
+    capacity INT NOT NULL,
+
+    UNIQUE (route_id, trip_day)
+);
+
+CREATE TABLE IF NOT EXISTS reservation (
+    id SERIAL PRIMARY KEY,
+    trip_id INT NOT NULL REFERENCES trip(id) ON DELETE CASCADE,
+    user_id INT NOT NULL REFERENCES app_user(id) ON DELETE CASCADE,
+);
